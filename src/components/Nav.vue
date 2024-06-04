35e93849a33ae8929b2904bae1ee52c1fb095480
Louca <baigneres.louca@gmail.com>
2024-06-04 03:36:59 +0200

    💄 Add SearchBar UI

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -3,31 +3,25 @@ import { RouterLink } from "vue-router"
 </script>
 
 <template>
-  <nav class="absolute bottom-0 w-full bg-gray-800 py-3 text-center">
-    <div class="grid grid-cols-3">
+  <nav class="absolute bottom-0 w-full bg-gray-800 text-center">
+    <div class="grid grid-cols-3 p-3 text-gray-400">
       <RouterLink to="/favorites" class="flex flex-col items-center gap-2">
-        <span
-          class="material-symbols-outlined rounded-3xl px-5 py-1 text-gray-400"
-        >
+        <span class="material-symbols-outlined rounded-3xl px-5 py-1">
           star
         </span>
-        <span class="text-sm text-gray-400"> Favorites </span>
+        <span class="text-sm"> Favorites </span>
       </RouterLink>
       <RouterLink to="/" class="flex flex-col items-center gap-2">
-        <span
-          class="material-symbols-outlined rounded-3xl px-5 py-1 text-gray-400"
-        >
+        <span class="material-symbols-outlined rounded-3xl px-5 py-1">
           schedule
         </span>
-        <span class="text-sm text-gray-400"> Recents </span>
+        <span class="text-sm"> Recents </span>
       </RouterLink>
       <RouterLink to="/contacts" class="flex flex-col items-center gap-2">
-        <span
-          class="material-symbols-outlined rounded-3xl px-5 py-1 text-gray-400"
-        >
+        <span class="material-symbols-outlined rounded-3xl px-5 py-1">
           group
         </span>
-        <span class="text-sm text-gray-400"> Contacts </span>
+        <span class="text-sm"> Contacts </span>
       </RouterLink>
     </div>
     <div class="flex w-full justify-center py-2">
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="flex items-center justify-between p-3 pb-2">
+    <div
+      class="flex h-10 w-full items-center gap-2 rounded-full bg-gray-800 px-3 text-gray-400"
+    >
+      <span class="material-symbols-outlined text-xl"> search </span>
+      <input
+        type="text"
+        placeholder="Search contacts"
+        class="w-full text-ellipsis border-none bg-transparent placeholder:text-gray-400 focus:outline-none"
+      />
+      <span class="material-symbols-outlined text-xl"> mic </span>
+      <span class="material-symbols-outlined text-xl"> more_vert </span>
+    </div>
+  </section>
+</template>
