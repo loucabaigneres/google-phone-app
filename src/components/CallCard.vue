f88a5951e13a1e07f1c57120b63235ac90947413
Louca <baigneres.louca@gmail.com>
2024-06-05 02:15:37 +0200

    💄 Create HomeView & CallCard

---
+++ src/components/CallCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex items-center gap-3">
+    <div
+      class="flex h-7 w-7 items-center justify-center rounded-full bg-orange-500"
+    >
+      <h3 class="text-lg text-gray-950">M</h3>
+    </div>
+    <div class="flex flex-col">
+      <h5 class="text-sm">John Doe</h5>
+      <div class="flex items-center gap-1">
+        <span class="material-symbols-outlined text-xs"> call_made </span>
+        <p class="text-xs">Mobile • 17:58</p>
+      </div>
+    </div>
+    <div class="ml-auto flex h-7 w-7 items-center justify-center rounded-full">
+      <span class="material-symbols-outlined text-xl"> call </span>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,28 @@
+<script setup>
+import CallCard from "@/components/CallCard.vue"
+</script>
+
 <template>
-  <section>
-    <h1>This is the recent calls page</h1>
+  <section
+    class="no-scrollbar flex h-full flex-col gap-4 overflow-y-auto px-3 py-1 text-gray-400"
+  >
+    <h4 class="text-xs">Today</h4>
+    <div class="flex flex-col gap-4">
+      <CallCard />
+      <CallCard />
+      <CallCard />
+    </div>
+    <h4 class="text-xs">Yesterday</h4>
+    <div class="flex flex-col gap-4">
+      <CallCard />
+      <CallCard />
+      <CallCard />
+    </div>
+    <h4 class="text-xs">Older</h4>
+    <div class="flex flex-col gap-4">
+      <CallCard />
+      <CallCard />
+      <CallCard />
+    </div>
   </section>
 </template>
